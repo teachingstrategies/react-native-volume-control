e23a005b97568511b5b9611e2ced3814c3d4b483
Tyler Malone <rtmalone@users.noreply.github.com>
2019-03-20 14:45:02 -0400

    [Android] Adds getVolume method (#2)

--- android/src/main/java/com/tavernari/volumecontroller/ReactNativeVolumeControllerModule.java
+++ android/src/main/java/com/tavernari/volumecontroller/ReactNativeVolumeControllerModule.java
@@ -1,81 +1,70 @@
 package com.tavernari.volumecontroller;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.media.AudioManager;
+
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-
-import android.media.AudioManager;
-import android.view.KeyEvent;
 
 public class ReactNativeVolumeControllerModule extends ReactContextBaseJavaModule {
 
-    private ReactApplicationContext context;
-    private float max_volume = (float) 0.0;
-    private AudioManager audioManager;
+  private ReactApplicationContext context;
+  private float max_volume = (float) 0.0;
+  private AudioManager am;
 
-    public ReactNativeVolumeControllerModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-      this.context = reactContext;
+  public ReactNativeVolumeControllerModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.context = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "ReactNativeVolumeController";
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+
+    try {
+      am = (AudioManager) this.context.getSystemService(Context.AUDIO_SERVICE);
+      max_volume = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+    } catch (Exception e) {
+      Log.e("ERROR", e.getMessage());
     }
+  }
 
+  public void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
+    this.context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+  }
 
-    @Override public String getName() {
-      return "ReactNativeVolumeController";
-    }
+  @ReactMethod
+  public void getVolume(Promise promise) {
+    promise.resolve(getNormalizedVolume());
+  }
 
-    @Override public void initialize() {
-      super.initialize();
+  @ReactMethod
+  public void change(float volume) {
+    am.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (volume * max_volume), 0);
+  }
 
-      try {
-        audioManager = (AudioManager) this.context.getSystemService(Context.AUDIO_SERVICE);
-        max_volume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-      } catch (Exception e) {
-        Log.e("ERROR", e.getMessage());
-      }
-    }
+  @ReactMethod
+  public void update() {
+    float volume = am.getStreamVolume(AudioManager.STREAM_MUSIC) / max_volume;
+    WritableMap params = Arguments.createMap();
+    params.putString("volume", String.valueOf(volume));
+    sendEvent(this.context, "VolumeControllerValueUpdatedEvent", params);
+  }
 
-
-    public void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
-      this.context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-          .emit(eventName, params);
-    }//VolumeControllerValueUpdatedEvent
-
-    @ReactMethod public void change(float volume) {
-      audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (volume*max_volume), 0);
-    }
-
-    /*public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_VOLUME_UP:
-                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
-                        AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
-                return true;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
-                        AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
-                return true;
-            default:
-                return false;
-        }
-    }*/
-
-    @ReactMethod public void update() {
-      float volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)/max_volume;
-      WritableMap params = Arguments.createMap();
-      params.putString("volume", String.valueOf(volume));
-      sendEvent(this.context, "VolumeControllerValueUpdatedEvent", params);
-    }
+  private float getNormalizedVolume() {
+    return am.getStreamVolume(AudioManager.STREAM_MUSIC) * 1.0f / am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+  }
 }
