a3295367ca4e6726a0f74021a8253785acbb51d1
Tyler Malone <tyler@pylon.com>
2019-03-27 13:54:00 -0400

    Adds events type to declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,15 @@
+import { NativeEventEmitter } from "react-native";
+
 export = RNVolumeControl;
 export as namespace RNVolumeControl;
 
 declare namespace RNVolumeControl {
   export function change(value: number): void;
   export function getVolume(): number;
+
+  export type VolumeEventType = {
+    volume: number;
+  };
+
+  export class RNVCEvents extends NativeEventEmitter {}
 }
